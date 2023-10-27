dcec47ec0e5997eb35bf91ca63a4c74b6b992ee3
Fred Sheppard <frederickjssheppard@gmail.com>
2023-10-27 23:54:16 +0100

    Add comments

--- recursion.asm
+++ recursion.asm
@@ -1,65 +1,73 @@
-! An implementation of the recursive function f(x) = 2f(x-1) +1, f(1) = 2
+! An implementation of the recursive function f(x) = 2*f(x-1) + 1, f(1) = 2
+! Recursion is implemented using a stack pointer
 
 .begin
 .org 2048
 
 ! ---- MACROS ----
+
+! Push a register's contents to the stack
 .macro push reg
-	sub %r14, 4, %r14
+	add %r14, 4, %r14
 	st reg, %r14
 .endmacro
 
+! Pop from the stack into a register
 .macro pop reg
-	ld %r14, reg	
-	add %r14, 4, %r14
+	ld %r14, reg
+	sub %r14, 4, %r14
 .endmacro
 
 ! ---- MEMORY ----
+	! User input
 	x: 3
+	! Displayed output
 	out: 0
 
-
-! ---- REGISTERS ----
-! r1 = Address
-! r2 = Value
-
 ! ---- MAIN ----
+
 main:
+	! Start the stack at location 3000
 	mov 3000, %r14
-	mov 3, %r2
+	! Push x to the stack
+	ld [x], %r2
 	push %r2
-	call f1
+	! Call the recursive function f
+	call f
+	! Pop and display the result
 	pop %r2
+	st %r2, [out]
 	halt
 
-f1:
-	! Get value, push pointer
+f:
+	! Pop x, store function pointer
 	pop %r2
 	push %r15
-	! Branch on val = 1
+	! If x == 1, branch
 	cmp %r2, 1
 	be one
-
-	! Decrease, push and call
+	! Decrement x, push x, call f
 	dec %r2
 	push %r2
-	call f1
-
-	Pop, multiply by 2, add 1
+	call f
+	! Pop the result of calling f
 	pop %r2
+	! Multiply x by 2
 	sll %r2, 1, %r2
+	! Increment x by 1
 	inc %r2
-	! Pop pointer, push val, jump
+	! Pop function pointer
 	pop %r1
+	! Push the result
 	push %r2
+	! Jump to calling function
 	jmpl %r1 + 4, %r0
 
-	
 one:
+	! Push 2 and jump to calling function
 	pop %r1
 	mov 2, %r2
 	push %r2
 	jmpl %r1 + 4, %r0
-
 .end
 
